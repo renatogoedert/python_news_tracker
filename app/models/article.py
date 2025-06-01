ca87e1642fb56ccf1fa1938b875b4ae5dd83100c
Renato Francisco Goedert <renato.goedert@gmail.com>
2025-06-01 17:21:59 +0100

    adding small fix

--- app/models/article.py
+++ app/models/article.py
@@ -12,10 +12,11 @@ class ArticleModel(db.Model):
     keywords = db.Column(db.ARRAY(db.String))
     created_at = db.Column(db.DateTime, default=db.func.now())
 
-    def __init__(self, url, title, author=None, published_date=None, summary=None, content=None, source=None, category=None, keywords=None, language=None):
-        self.url = url
-        self.title = title
-        self.author = author
-        self.published_date = published_date
-        self.content = content
-        self.keywords = keywords
+    # Comment out init method as there is no specific initialization logic
+    # def __init__(self, url, title, author=None, published_date=None, summary=None, content=None, source=None, category=None, keywords=None, language=None):
+    #     self.url = url
+    #     self.title = title
+    #     self.author = author
+    #     self.published_date = published_date
+    #     self.content = content
+    #     self.keywords = keywords
