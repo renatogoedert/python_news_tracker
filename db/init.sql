e370a40107115bbbecdca6e0f07803c8b854a9ef
Renato Francisco Goedert <renato.goedert@gmail.com>
2025-06-02 14:07:19 +0100

    creating init.sql

---
+++ db/init.sql
@@ -0,0 +1,20 @@
+-- Create 'authors' table if it doesn't exist
+CREATE TABLE IF NOT EXISTS authors (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) UNIQUE NOT NULL,
+    keywords TEXT[],
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Create 'articles' table if it doesn't exist
+CREATE TABLE IF NOT EXISTS articles (
+    id SERIAL PRIMARY KEY,
+    url VARCHAR(255) UNIQUE NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    author_id INT REFERENCES authors(id) ON DELETE SET NULL,
+    published_date TIMESTAMP,
+    content TEXT,
+    keywords TEXT[],
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
