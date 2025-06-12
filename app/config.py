e061bd1fe8e9a04f5df4eded0ceb2c92f5ca0077
Renato Francisco Goedert <renato.goedert@gmail.com>
2025-06-12 20:06:25 +0100

    replacing DB  location on config.py

--- app/config.py
+++ app/config.py
@@ -2,5 +2,5 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'postgresql://admin_username:admin_password@localhost:5432/news_db')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'postgresql://admin_username:admin_password@db:5432/news_db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
