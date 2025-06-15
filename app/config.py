c9b0d395dc82644b989caa831f2d3fe6f5f4f8f3
Renato Francisco Goedert <renato.goedert@gmail.com>
2025-06-15 17:47:44 +0100

    updating config.py with .env

--- app/config.py
+++ app/config.py
@@ -1,6 +1,16 @@
 #configuration settings, could have diferent confs for dev, test, and prod
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'postgresql://admin_username:admin_password@db:5432/news_db')
+    DB_USERNAME = os.getenv("POSTGRES_USER", "admin_username")
+    DB_PASSWORD = os.getenv("POSTGRES_PASSWORD", "admin_password")
+    DB_NAME     = os.getenv("POSTGRES_DB", "news_db")
+
+    SQLALCHEMY_DATABASE_URI = (
+        f"postgresql://{DB_USERNAME}:{DB_PASSWORD}"
+        f"@db:5432/{DB_NAME}"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
